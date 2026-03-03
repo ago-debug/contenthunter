72f9da956cd3d632fe1a4a482bc599b49c5ec3ce
Augusto Genca <augustogenca@MacBook-Air-di-Augusto.local>
2026-03-03 18:56:48 +0100

    Fix: Public access for storage API to resolve PDF loading error

--- middleware.ts
+++ middleware.ts
@@ -6,6 +6,10 @@ export default withAuth({
     },
     callbacks: {
         authorized: ({ req, token }) => {
+            // Allow public access to storage and pdfs
+            if (req.nextUrl.pathname.startsWith("/api/storage")) return true;
+            if (req.nextUrl.pathname.startsWith("/api/proxy-image")) return true;
+
             if (req.nextUrl.pathname.startsWith("/api/") && !token) {
                 return false; // Result in 401 if requested from withAuth middleware for APIs
             }
@@ -27,7 +31,8 @@ export const config = {
          * - favicon.ico (favicon file)
          * - api/fix-auth (auto-fix plesk dev env)
          * - uploads (static files)
+         * - api/storage (forced public for pdf worker)
          */
-        "/((?!api/register|api/auth|api/fix-auth|login|register|_next/static|_next/image|favicon.ico|uploads/).*)",
+        "/((?!api/register|api/auth|api/fix-auth|api/storage|login|register|_next/static|_next/image|favicon.ico|uploads/).*)",
     ],
 };
